33f12e04f5ea1e6b987847b042ad14306e4efc03
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-06-25 19:41:36 +0700

    Viet lai noi dung trang gioi thieu

--- resources/views/client/pages/introduce.blade.php
+++ resources/views/client/pages/introduce.blade.php
@@ -5,17 +5,12 @@
         <div class="d-flex flex-xl-row flex-column">
             <div class="col-xl-5 col-12">
                 <h2 class="fw-bold fs-1">
-                    Where Beauty
-                    <br>
-                    Meets Functionality
+                    Chào mừng bạn đến với Patina!
                 </h2>
             </div>
             <div class="col-xl-7 col-12 pt-3 pt-xl-0">
                 <p class="m-0 ps-2" style="color: var(--secondary-900-color);font-size: 18px;">
-                    Lorem ipsum dolor sit amet consectetur. Blandit at maecenas dui sed amet sit enim vitae. Amet purus
-                    dictum urna sagittis dignissim. At fermentum nisl ullamcorper orci. Pellentesque id tempor lacus
-                    aliquet tempus vitae nibh habitasse consectetur. Augue quis cras blandit habitant neque. Faucibus
-                    vestibulum id nisi ligula ultricies et vehicula
+                    Patina Fashion Store là điểm đến lý tưởng cho những ai yêu thích phong cách thanh lịch và sự độc đáo. Với không gian trưng bày tinh tế, từng món đồ tại Patina đều mang đậm dấu ấn của sự sang trọng và sự cầu kỳ trong từng chi tiết. Từ những bộ cánh dành cho công sở đến những trang phục dạo phố, mỗi sản phẩm tại cửa hàng đều được chọn lọc kỹ càng, đáp ứng được mọi sở thích và nhu cầu của các tín đồ thời trang. Patina không chỉ là nơi mua sắm mà còn là điểm hẹn lý tưởng để khám phá và thể hiện phong cách cá nhân một cách hoàn hảo.
                 </p>
             </div>
         </div>
@@ -41,7 +36,7 @@
         <div class="row g-2 my-5">
             <div class="col-12 col-xl-3 text-center">
                 <img class="img-thumbnail img-our-team" src="img/ourteam.jpg" alt="">
-                <h6>Anh Buoi</h6>
+                <h6>a</h6>
                 <p class="m-0" style="color: #00051654;">District Creative Producer</p>
                 <div class="d-flex justify-content-center">
                     <div class="icon-footer">
@@ -57,7 +52,7 @@
             </div>
             <div class="col-12 col-xl-3 text-center">
                 <img class="img-thumbnail img-our-team" src="img/ourteam.jpg" alt="">
-                <h6>Anh Buoi</h6>
+                <h6>b</h6>
                 <p class="m-0" style="color: #00051654;">District Creative Producer</p>
                 <div class="d-flex justify-content-center">
                     <div class="icon-footer">
@@ -73,7 +68,7 @@
             </div>
             <div class="col-12 col-xl-3 text-center">
                 <img class="img-thumbnail img-our-team" src="img/ourteam.jpg" alt="">
-                <h6>Anh Buoi</h6>
+                <h6>c</h6>
                 <p class="m-0" style="color: #00051654;">District Creative Producer</p>
                 <div class="d-flex justify-content-center">
                     <div class="icon-footer">
@@ -89,7 +84,7 @@
             </div>
             <div class="col-12 col-xl-3 text-center">
                 <img class="img-thumbnail img-our-team" src="img/ourteam.jpg" alt="">
-                <h6>Anh Buoi</h6>
+                <h6>d</h6>
                 <p class="m-0" style="color: #00051654;">District Creative Producer</p>
                 <div class="d-flex justify-content-center">
                     <div class="icon-footer">
