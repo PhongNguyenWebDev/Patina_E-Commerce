5325560e4e9c420dcdfcbfd0448e3ee6e754df67
Nguyen Duong Tuan <along18901@gmail.com>
2024-08-23 22:04:45 +0700

    update admin coupon

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Coupon extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'coupons';
     protected $fillable = [
         'code',
@@ -24,6 +25,7 @@ class Coupon extends Model
         'start_date',
         'end_date',
     ];
+    protected $dates = ['deleted_at'];
     public function userCoupons()
     {
         return $this->hasMany(UserCoupon::class);
